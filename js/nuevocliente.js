490aff84c0a31ceefaf73ca2cab86f86f9d44abe
Christian Morales <chrisrodo84@gmail.com>
2021-07-03 01:09:33 -0500

    Proyecto completado con uso de IndexedDB y JavaScript

---
+++ js/nuevocliente.js
@@ -0,0 +1,56 @@
+(function() {
+    let DB;
+    const formulario = document.querySelector('#formulario');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        conectarDB();
+
+        formulario.addEventListener('submit', validarCliente);
+    });
+
+    
+
+    function validarCliente(e) {
+        e.preventDefault();
+        
+        //Leer los inputs
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        if (nombre === '' || email === '' || telefono === '' || empresa === '') {
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+            return;
+        }
+
+        //Crear un objeto con la información
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa,
+            id: Date.now()
+        }
+
+        crearNuevoCliente(cliente);
+    }
+
+    function crearNuevoCliente(cliente) {
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.add(cliente);
+        transaction.onerror = function() {
+            imprimirAlerta('Hubo un error', 'error');
+        }
+        transaction.oncomplete = function() {
+            imprimirAlerta('Cliente agregado correctamente', 'correcto');
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        }
+    }
+
+    
+})();
